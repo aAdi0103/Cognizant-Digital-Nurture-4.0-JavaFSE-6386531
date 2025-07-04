75358b110ce4d29da2e3bfe175d0bcd0f40f03cb
Aditya kumar <ak0755591@gmail.com>
2025-07-04 12:18:34 +0530

    Improved Depenency Injection

--- Week3/Implementing_Dependency_Injection/src/main/java/com/LibManagement/Main.java
+++ Week3/Implementing_Dependency_Injection/src/main/java/com/LibManagement/Main.java
@@ -1,5 +1,6 @@
 package com.LibManagement;
 
+import com.LibManagement.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        BookService service = context.getBean("bookService", BookService.class);
-        service.bookService();
+        BookService bookService=(BookService) context.getBean("bookService");
+        bookService.addBook("SpringBoot Books");
     }
 }
